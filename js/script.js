fffec6a6cf9e507683289bbca690d767de5ad405
Eddie Jr <eddyfhizzy2000@gmail.com>
2022-03-30 15:51:50 +0100

    update on cartline landing

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const menuToggle = document.querySelector(".toggle");
+const showcase = document.querySelector(".showcase");
+
+menuToggle.addEventListener("click", () => {
+    menuToggle.classList.toggle("active");
+    showcase.classList.toggle("active");
+});
+
+
+//  dynamic accordion
+/*=============== ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.accordion__item')
+
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('.accordion__header')
+    accordionHeader.addEventListener('click', () => {
+
+        const openItem = document.querySelector('.accordion-open')
+        toggleItem(item)
+        if (openItem && openItem !== item) {
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector('.accordion__content')
+
+    if (item.classList.contains('accordion-open')) {
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    } else {
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+}
+
+console.log("testLogin")
